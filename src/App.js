4677d4a29339cb8ecd6fa90799dc859f648d49cb
sachin chaurasiya <schaurasiya470@gmail.com>
2026-06-27 12:52:49 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
-import GithubProjects from "./components/GithubProjects";
+// import GithubProjects from "./components/GithubProjects";
 import Journey from "./components/Journey";
 import Contact from "./components/Contact";
 
